75f5db3ef78ff83db57862d3a3d9b90624699d4e
Ben Smith <benjaminsmith1981@gmail.com>
2018-03-09 18:51:39 +0100

    Initial Commit

---
+++ SwiftyJSONDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyJSONDemo
+//
+//  Created by Ben Smith on 09/03/2018.
+//  Copyright © 2018 Ben Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftyJSONDemoTests/SwiftyJSONDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftyJSONDemoTests.swift
+//  SwiftyJSONDemoTests
+//
+//  Created by Ben Smith on 09/03/2018.
+//  Copyright © 2018 Ben Smith. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyJSONDemo
+
+class SwiftyJSONDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
